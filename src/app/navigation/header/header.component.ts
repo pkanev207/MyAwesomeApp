c72e9de3479ce2c45c7d1f98e148102dc0fba1da
Petar <kanev_petar@yahoo.com>
2022-12-02 22:42:46 +0200

    navigation

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
+
+
+  onLogout() {
+    // this.authService.logout();
+  }
+
+
+  ngOnDestroy(): void { }
 
 }
--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
+  @Output() closeSidenav = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription?: Subscription;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    // this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    // this.authSubscription?.unsubscribe();
   }
 
 }
