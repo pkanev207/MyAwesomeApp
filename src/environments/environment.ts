e26f832ecc8f549426987a7524e52e3ecc543638
Petar <kanev_petar@yahoo.com>
2022-12-18 04:29:27 +0200

    app deployed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,14 +15,6 @@ export const environment = {
   },
 };
 
-// APP_PROJECT_ID=myproject-8a6c3,
-// APP_APP_ID=1:702941285853:web:dd45a3a543496108203a34,
-// APP_STORAGE_BUCKET=myproject-8a6c3.appspot.com,
-// APP_API_KEY=AIzaSyDG8QvWZG1NBwqXy_AuTTjdVc7Q0_b44WI,
-// APP_AUTH_DOMAIN=myproject-8a6c3.firebaseapp.com,
-// APP_MESSAGING_SENDER_ID=702941285853,
-// APP_MEASUREMENT_ID=G-TN1Q24TBJW,
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
